c40bfd7b1f3947d23c1959fec2289b0b5ce51ec8
Nambyungwook <skaquddnr21@gmail.com>
2020-01-21 21:39:59 +0900

    ch1~ch2

---
+++ src/test/java/com/nbw/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,43 @@
+package com.nbw.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)//테스트 진행시 JUnit에 내장된 실행자 외에 다른 실행자를 실행->SpringRunner.class(스프링부트 테스트와 JUnit을 연결)
+@WebMvcTest//웹에 집중, 컨트롤러는 가능/ 서비스, 컴포넌트, 레파지토리는 불가
+public class HelloControllerTest {
+
+    @Autowired//스프링이 관리하는 빈을 주입
+    private MockMvc mvc;//웹 api테스트용, 스프링 mvc테스트의 시작, GET,POST등의 api 테스트를 가능하게 함
+
+    @Test
+    public void hello_return() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto_return() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+    }
+}
