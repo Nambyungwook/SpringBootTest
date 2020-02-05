0abfff92c128b5776919a304ca3ad6912598562a
Nambyungwook <skaquddnr21@gmail.com>
2020-02-05 20:49:18 +0900

    스프링부트와 JPA로 게시판 구현

---
+++ src/main/java/com/nbw/book/springboot/web/PostsApiController.java
@@ -0,0 +1,44 @@
+package com.nbw.book.springboot.web;
+
+import com.nbw.book.springboot.service.posts.PostsService;
+import com.nbw.book.springboot.web.dto.PostsListResponseDto;
+import com.nbw.book.springboot.web.dto.PostsResponseDto;
+import com.nbw.book.springboot.web.dto.PostsSaveRequestDto;
+import com.nbw.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
+}
