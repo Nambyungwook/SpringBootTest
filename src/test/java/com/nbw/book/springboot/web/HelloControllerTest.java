bca6cf0fa568d063914456200e7e197103337e11
Nambyungwook <skaquddnr21@gmail.com>
2020-02-08 21:10:15 +0900

    ch05완료

--- src/test/java/com/nbw/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/nbw/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.nbw.book.springboot.web;
 
+import com.nbw.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +16,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)//테스트 진행시 JUnit에 내장된 실행자 외에 다른 실행자를 실행->SpringRunner.class(스프링부트 테스트와 JUnit을 연결)
-@WebMvcTest//웹에 집중, 컨트롤러는 가능/ 서비스, 컴포넌트, 레파지토리는 불가
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)//웹에 집중, 컨트롤러는 가능/ 서비스, 컴포넌트, 레파지토리는 불가
 public class HelloControllerTest {
 
     @Autowired//스프링이 관리하는 빈을 주입
     private MockMvc mvc;//웹 api테스트용, 스프링 mvc테스트의 시작, GET,POST등의 api 테스트를 가능하게 함
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello_return() throws Exception {
         String hello = "hello";
@@ -27,6 +36,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto_return() throws Exception {
         String name = "hello";
