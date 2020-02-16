7350f5e6a70b83f5af0765510e573457d9285afa
Nambyungwook <skaquddnr21@gmail.com>
2020-02-16 14:55:59 +0900

    test

--- src/main/java/com/nbw/book/springboot/web/HelloController.java
+++ src/main/java/com/nbw/book/springboot/web/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //컨트롤러가 json을 반환하는 컨트롤러로 만듬
+@RestController
 public class HelloController {
 
     @GetMapping("/hello") //http 메소드인 get요청을 받을 수 있는 api를 만들어줌
